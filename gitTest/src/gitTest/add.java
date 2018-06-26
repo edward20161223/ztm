20a871f8a2562eccff728f54e028161c1de1b2ef
ztm <1506483623@qq.com>
2018-06-26 11:35:05 +0800

    添加了add.java文件

---
+++ gitTest/src/gitTest/add.java
@@ -0,0 +1,17 @@
+package gitTest;
+
+public class add {
+
+	public add() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int i=19;
+		int j=20;
+		int result=0;
+		result=i+j;
+		System.out.println("result is:"+result);
+	}
+
+}

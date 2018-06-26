f78c284875970c9d8bf2ab290fa2a46ab37b47b2
ztm <1506483623@qq.com>
2018-06-26 10:43:32 +0800

    提交gitTest项目

---
+++ gitTest/src/gitTest/test.java
@@ -0,0 +1,14 @@
+package gitTest;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+
+	}
+
+}

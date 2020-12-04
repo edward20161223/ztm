4fba7f137d248efc8546f98214f528a9113dcf60
ztm <1506483623@qq.com>
2020-12-04 15:55:32 +0800

    20201204提交之前修改内容

--- springBootTest/src/main/java/springBootTest/frist/Application.java
+++ springBootTest/src/main/java/springBootTest/frist/Application.java
@@ -6,17 +6,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 
 /*
  * 使用springboot，使用springboot自带的tomcat，如果项目引入自己的tomcat，会引起包冲突，需要删除掉应用的tomcat
  */
 
+
+/*
+ * 映射访问与对象注入都与文件位置的关系，springboot项目的目录结构，springboot组件扫描的原理。20191217
+ * @Controller  @RequestMapping("/test")  @ResponseBody  ==  @RestController  @RequestMapping("/calculationAdd")
+ */
+
+
 @SpringBootApplication
 @Controller
 @EnableAutoConfiguration
 public class Application {
-    @RequestMapping("/")
+	
+    @RequestMapping("/test")
     @ResponseBody
     public String index() {
         return "Hello World!";
@@ -25,5 +35,16 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
+    
+//    @RequestMapping("/hello")
+//    @ResponseBody
+//    public String sayHello(){
+//        return "hello";
+//    }
+    
+    
 }
+
+
+
